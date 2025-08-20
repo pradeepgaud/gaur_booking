11c218daceaf264273c3af240ff6ac32236a93fe
pradeepgaud <pradeepgaur1825@gmail.com>
2025-08-20 12:00:44 +0530

    Update true AuthController.js

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -15,8 +15,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       // secure:process.env.NODE_ENVIRONMENT ==="production",
-      secure:"None",
-      sameSite: true,
+      secure:true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
     return res.status(200).json(user)
@@ -43,8 +43,8 @@ export const login = async (req, res) => {
     let token = await getToken(user._id)
     res.cookie("token", token, {
       httpOnly: true,
-      secure: "None", // Fixed!
-      sameSite: true,
+      secure: true, // Fixed!
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
     return res.status(200).json(user)
